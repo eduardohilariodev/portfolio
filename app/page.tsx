46fffd7b48487ca4f4432f48292e68ca7608163f
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-11-04 21:39:31 -0300

    feat: add magic button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { FaHome } from "react-icons/fa";
 import Hero from "../components/Hero";
+import { FloatingNav } from "../components/ui/floating-navbar";
 
 export default function Home() {
   return (
-    <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
       </div>
     </main>
