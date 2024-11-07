a13c7533a5ff62ce14a506ba7e2c55609afa1d73
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-11-07 19:56:02 -0300

    feat: add recent-projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import { FaHome } from "react-icons/fa";
 import Grid from "../components/grid";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
+import RecentProjects from "../components/recent-projects";
 import { FloatingNav } from "../components/ui/floating-navbar";
+import { navItems } from "../data";
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
