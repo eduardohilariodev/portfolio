3fc4f76b38f447538b7bde396a5b754f221cf82b
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-02-05 21:10:14 -0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,16 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Archivo } from "next/font/google";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { ThemeProvider } from "./components/ThemeProvider";
+import { Archivo, Cormorant } from "next/font/google";
 import "./globals.css";
 
-import localFont from "next/font/local";
-
-const garamond = localFont({
-  variable: "--font-garamond",
-  src: [
-    {
-      path: "./fonts/Garamond/ITC Garamond Std Light Narrow.otf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "./fonts/Garamond/ITC Garamond Std Book Narrow.otf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/Garamond/ITC Garamond Std Book.otf",
-      weight: "600",
-      style: "normal",
-    },
-  ],
+const archivo = Archivo({ subsets: ["latin"], variable: "--font-archivo" });
+const cormorant = Cormorant({
+  subsets: ["latin"],
+  variable: "--font-cormorant",
 });
-const archivo = Archivo({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Eduardo Hilario - Software Engineer",
@@ -44,8 +26,11 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={`${archivo.className} ${garamond.variable}`}
+      className={`${archivo.variable} ${cormorant.variable}`}
     >
+      <link rel="icon" href="/favicon.ico" sizes="32x32" />
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <body
         className={`min-h-screen bg-neutral-200 font-sans text-neutral-900 transition-colors dark:bg-neutral-900 dark:text-white`}
       >
