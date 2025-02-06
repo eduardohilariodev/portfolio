5d1e6b242b3f78e26a7ee32f4f9f9f7f0a1d0555
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-02-06 17:38:05 -0300

    refactor: update logo props

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 
-export default function Logo() {
+interface LogoProps {
+  width?: number;
+  height?: number;
+}
+
+export default function Logo({ width = 100, height = 100 }: LogoProps) {
   return (
     <Image
       src="/logo.svg"
       alt="Logo"
-      fill
       className="object-contain transition-colors dark:invert"
       priority
+      width={width}
+      height={height}
     />
   );
 }
