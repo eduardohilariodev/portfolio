209f5de2bbc80ca0df7f544bd5ab500a43bcc8c5
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2025-02-08 21:15:32 -0300

    feat: add redirect for resume

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "/Eduardo_Hilario_Software_Engineer_Resume.pdf",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
